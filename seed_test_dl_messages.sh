9fbec9ebad4523b1310202f4287ab6a5bdeeccd3
Grayson Kuhns <grayson@graysonkuhns.com>
2025-03-21 18:27:24 -0700

    create script for sending test messages to dead letter topic

---
+++ seed_test_dl_messages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+ENVIRONMENT=${ENVIRONMENT:-default}
+TOPIC_NAME="${ENVIRONMENT}-events-dead-letter"
+for i in {1..3}; do
+    MESSAGE="{\"message\": \"Test DL message $i\"}"
+    echo "Publishing message $i to topic ${TOPIC_NAME}"
+    gcloud pubsub topics publish "$TOPIC_NAME" --message "$MESSAGE"
+done
