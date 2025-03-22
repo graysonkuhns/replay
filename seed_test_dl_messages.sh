c218f968d2c32bbd512c650d3395039c6d75c55f
Grayson Kuhns <grayson@graysonkuhns.com>
2025-03-22 08:05:06 -0700

    seed 5 test messages

--- seed_test_dl_messages.sh
+++ seed_test_dl_messages.sh
@@ -2,7 +2,7 @@
 
 ENVIRONMENT=${ENVIRONMENT:-default}
 TOPIC_NAME="${ENVIRONMENT}-events-dead-letter"
-for i in {1..3}; do
+for i in {1..5}; do
     MESSAGE="{\"message\": \"Test DL message $i\"}"
     echo "Publishing message $i to topic ${TOPIC_NAME}"
     gcloud pubsub topics publish "$TOPIC_NAME" --message "$MESSAGE"
