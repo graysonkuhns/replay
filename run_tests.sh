e8c2af4ae0ec94057acac56b65f0b7dc4df5214f
Grayson Kuhns <grayson@graysonkuhns.com>
2025-05-20 08:05:54 -0500

    allow integration tests to run for 20 mins instead of 10

--- run_tests.sh
+++ run_tests.sh
@@ -16,10 +16,10 @@ fi
 # Check if a specific test name was provided
 if [ $# -eq 0 ]; then
   echo "Running all tests..."
-  go test -count 1 -v ./integration_tests
+  go test -count 1 -v -timeout 20m ./integration_tests
 else
   # Run only the specified test
   TEST_NAME=$1
   echo "Running test: $TEST_NAME"
-  go test -count 1 -v ./integration_tests -run "$TEST_NAME"
+  go test -count 1 -v -timeout 20m ./integration_tests -run "$TEST_NAME"
 fi
